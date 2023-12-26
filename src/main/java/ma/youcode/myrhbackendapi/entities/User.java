9286469801f28ad3f7d9c1187c9f25ccacc442b8
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-26 14:44:30 +0100

    add User abstract superclass + establishe database connection

---
+++ src/main/java/ma/youcode/myrhbackendapi/entities/User.java
@@ -0,0 +1,24 @@
+package ma.youcode.myrhbackendapi.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@MappedSuperclass
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+}
