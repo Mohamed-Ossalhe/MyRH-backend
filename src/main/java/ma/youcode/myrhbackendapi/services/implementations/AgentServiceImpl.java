de57772fbc606da26e8ecbffa62ed9580225204c
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-27 09:48:14 +0100

    add agent service implementation crud

--- src/main/java/ma/youcode/myrhbackendapi/services/implementations/AgentServiceImpl.java
+++ src/main/java/ma/youcode/myrhbackendapi/services/implementations/AgentServiceImpl.java
@@ -3,6 +3,8 @@ package ma.youcode.myrhbackendapi.services.implementations;
 import lombok.RequiredArgsConstructor;
 import ma.youcode.myrhbackendapi.dto.requests.AgentRequest;
 import ma.youcode.myrhbackendapi.dto.responses.AgentResponse;
+import ma.youcode.myrhbackendapi.entities.Agent;
+import ma.youcode.myrhbackendapi.exceptions.ResourceNotFoundException;
 import ma.youcode.myrhbackendapi.repositories.AgentRepository;
 import ma.youcode.myrhbackendapi.services.AgentService;
 import org.modelmapper.ModelMapper;
@@ -22,31 +24,53 @@ public class AgentServiceImpl implements AgentService {
     private final ModelMapper mapper;
     @Override
     public List<AgentResponse> getAll() {
-        return null;
+        List<Agent> agents = agentRepository.findAll();
+        if (agents.isEmpty())
+            throw new ResourceNotFoundException("No Agents Found");
+        return agents.stream().map(agent -> mapper.map(agent, AgentResponse.class)).toList();
     }
 
     @Override
     public Page<AgentResponse> getAll(Pageable pageable) {
-        return null;
+        Page<Agent> agentPage = agentRepository.findAll(pageable);
+        if (agentPage.isEmpty())
+            throw new ResourceNotFoundException("No Agents Found");
+        return agentPage.map(agent -> mapper.map(agent, AgentResponse.class));
     }
 
     @Override
-    public Optional<AgentResponse> find(UUID uuid) {
-        return Optional.empty();
+    public Optional<AgentResponse> find(UUID id) {
+        Optional<Agent> agent = agentRepository.findById(id);
+        if (agent.isEmpty())
+            throw new ResourceNotFoundException("No Agent Found with ID: " + id);
+        return Optional.of(mapper.map(agent, AgentResponse.class));
     }
 
     @Override
     public Optional<AgentResponse> create(AgentRequest agentRequest) {
-        return Optional.empty();
+        Agent agent = mapper.map(agentRequest, Agent.class);
+        Agent savedAgent = agentRepository.save(agent);
+        // TODO: add some validation if needed
+        return Optional.of(mapper.map(savedAgent, AgentResponse.class));
     }
 
     @Override
-    public Optional<AgentResponse> update(AgentRequest agentRequest, UUID uuid) {
-        return Optional.empty();
+    public Optional<AgentResponse> update(AgentRequest agentRequest, UUID id) {
+        Optional<Agent> agent = agentRepository.findById(id);
+        if (agent.isEmpty())
+            throw new ResourceNotFoundException("No Agent Found with ID: " + id);
+        // TODO: this is wrong fix it later!!!! by setting the request info to the found agent
+        Agent agentToUpdate = mapper.map(agentRequest, Agent.class);
+        Agent updatedAgent = agentRepository.save(agentToUpdate);
+        return Optional.of(mapper.map(updatedAgent, AgentResponse.class));
     }
 
     @Override
-    public boolean destroy(UUID uuid) {
-        return false;
+    public boolean destroy(UUID id) {
+        Optional<Agent> agent = agentRepository.findById(id);
+        if (agent.isPresent()) {
+            agentRepository.delete(agent.get());
+            return true;
+        }else throw new ResourceNotFoundException("No Agent Found with ID: " + id);
     }
 }
