31fb2f0ce40ad7904bff4d273b65d9437135bc8d
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-27 09:17:58 +0100

    add exceptions handler

---
+++ src/main/java/ma/youcode/myrhbackendapi/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package ma.youcode.myrhbackendapi.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> messages = new HashMap<>();
+        exception.getBindingResult()
+                .getAllErrors()
+                .forEach(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    messages.put(fieldName, errorMessage);
+                });
+        return new ResponseEntity<>(messages, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/ma/youcode/myrhbackendapi/services/implementations/AgentServiceImpl.java
+++ src/main/java/ma/youcode/myrhbackendapi/services/implementations/AgentServiceImpl.java
@@ -1,16 +1,25 @@
 package ma.youcode.myrhbackendapi.services.implementations;
 
+import lombok.RequiredArgsConstructor;
 import ma.youcode.myrhbackendapi.dto.requests.AgentRequest;
 import ma.youcode.myrhbackendapi.dto.responses.AgentResponse;
+import ma.youcode.myrhbackendapi.repositories.AgentRepository;
 import ma.youcode.myrhbackendapi.services.AgentService;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
+@RequiredArgsConstructor
 public class AgentServiceImpl implements AgentService {
+
+    private final AgentRepository agentRepository;
+    private final ModelMapper mapper;
     @Override
     public List<AgentResponse> getAll() {
         return null;
