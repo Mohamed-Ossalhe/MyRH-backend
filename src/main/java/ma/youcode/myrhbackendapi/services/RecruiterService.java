39efd169eaadc88553cad6baeb32c56cc63fd604
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2024-01-01 16:14:38 +0100

    add security + configure security and add neccesary beans

--- src/main/java/ma/youcode/myrhbackendapi/services/RecruiterService.java
+++ src/main/java/ma/youcode/myrhbackendapi/services/RecruiterService.java
@@ -2,9 +2,12 @@ package ma.youcode.myrhbackendapi.services;
 
 import ma.youcode.myrhbackendapi.dto.requests.RecruiterRequest;
 import ma.youcode.myrhbackendapi.dto.responses.RecruiterResponse;
+import ma.youcode.myrhbackendapi.entities.Recruiter;
 import ma.youcode.myrhbackendapi.interfaces.CrudInterface;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RecruiterService extends CrudInterface<RecruiterResponse, RecruiterRequest, UUID> {
+    public Recruiter findRecruiterByEmail(String email);
 }
